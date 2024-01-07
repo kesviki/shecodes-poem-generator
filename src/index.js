dff9db5f255aecb15cce8c76c08097500c542641
kesviki <keseruviktoria@icloud.com>
2024-01-07 21:04:06 +0100

    Integrated SheCodes AI API, added JS AI integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 function generatePoem(event) {
     event.preventDefault();
-  
-    new Typewriter("#poem", {
-      strings: "This is the poem",
-      autoStart: true,
-      delay: 1,
-      cursor: "",
-    });
+
+    let poemProgress = document.querySelector("#poem");
+    poemProgress.innerHTML = "Your poem is being generated";
+
+    let instructionsInput = document.querySelector("#user-instructions")
+    let apiKey = "aofa346fccet13abb06d833e45790bfc";
+    let context =
+      "you are a professional poet who can write a poem based on any prompt in basic HTMl. Do not include a title for the poem. Make sure to follow the instructions"
+       let prompt = `write a four line long poem about ${instructionsInput.value}`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    axios.get(apiUrl).then(typePoem);
+    
+    function typePoem(response) {
+      new Typewriter("#poem", {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+    }
   }
   
   let poemFormElement = document.querySelector("#poem-generator-form");
   poemFormElement.addEventListener("submit", generatePoem);
+
