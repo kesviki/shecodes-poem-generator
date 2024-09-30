c78f97c83a96f666d65e250eb5ba4f9c0d3d50df
kesviki <keseruviktoria@icloud.com>
2024-09-30 18:30:56 +0200

    Hid the box before poem generation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 function generatePoem(event) {
     event.preventDefault();
 
-    let poemProgress = document.querySelector("#poem");
-    poemProgress.innerHTML = "Your poem is being generated";
+
 
     let instructionsInput = document.querySelector("#user-instructions")
     let apiKey = "aofa346fccet13abb06d833e45790bfc";
     let context =
       "you are a professional poet who can write a poem based on any prompt in basic HTMl. Do not include a title for the poem. Make sure to follow the instructions"
        let prompt = `write a four line long poem about ${instructionsInput.value}`;
+    let poemProgress = document.querySelector("#poem");
+    poemProgress.innerHTML = `Your poem about ${instructionsInput.value} is being generated`;
+
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     axios.get(apiUrl).then(typePoem);
     
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+
     function typePoem(response) {
       new Typewriter("#poem", {
         strings: response.data.answer,
